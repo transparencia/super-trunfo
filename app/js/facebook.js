343b16d46107184f252b7bbbd0196d80813aa8bd
João Batista Neto <neto.joaobatista@gmail.com>
2012-10-18 22:59:55 -0300

    Ajuste no link dos perfis para abrir fora do iframe e adição do facebook.js

---
+++ app/js/facebook.js
@@ -0,0 +1,23 @@
+FB.init({
+	appId : '369251863156901',
+	channelUrl : '//neto.my.phpcloud.com/super-trunfo/',
+	status : true,
+	cookie : true,
+	xfbml : true
+});
+
+FB.login(function(response) {
+	if (response.authResponse) {
+		FB.api('/me?fields=name,picture', function(response) {
+			console.log([response.name, response.picture.data.url]);
+			var img = $('.user .user-photo img');
+			
+			img.attr('alt', response.name);
+			img.attr('src', response.picture.data.url);
+			
+			$('.user .user-name').text(response.name);
+		});
+	} else {
+		//TODO: usuário não autorizou, o que fazemos?
+	}
+});
