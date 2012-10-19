12299fb08c43b4dbbc41ea22b6100dbe8528d372
João Batista Neto <neto.joaobatista@gmail.com>
2012-10-18 23:28:49 -0300

    Checagem do estado do login

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -1,29 +1,44 @@
-FB.init({
-	appId : '369251863156901',
-	channelUrl : '//neto.my.phpcloud.com/super-trunfo/',
-	status : true,
-	cookie : true,
-	xfbml : true
-});
+window.SUPERTRUNFO = window.SUPERTRUNFO || {};
+SUPERTRUNFO.APPS = SUPERTRUNFO.APPS || {};
 
-FB.Canvas.setSize({width: 850});
-
-FB.login(function(response) {
-	if (response.authResponse) {
-		FB.api('/me?fields=name,picture', function(response) {
-			var userProfile = function(base) {
-				var img = $(base + ' .user-photo img');
+SUPERTRUNFO.APPS.Facebook = {
+	login: login = function(response) {
+		if (response.authResponse) {
+			FB.api('/me?fields=name,picture', function(response) {
+				var userProfile = function(base) {
+					var img = $(base + ' .user-photo img');
+					
+					img.attr('alt', response.name);
+					img.attr('src', response.picture.data.url);
+					
+					$(base + ' .user-name').text(response.name);
+				};
 				
-				img.attr('alt', response.name);
-				img.attr('src', response.picture.data.url);
-				
-				$(base + ' .user-name').text(response.name);
-			};
-			
-			userProfile('.user');
-			userProfile('.score-me')
+				userProfile('.user');
+				userProfile('.score-me')
+			});
+		} else {
+			FB.login(SUPERTRUNFO.APPS.Facebook.login);
+		}
+	},
+	init: function() {
+		FB.init({
+			appId : '369251863156901',
 		});
-	} else {
-		//TODO: usuário não autorizou, o que fazemos?
+		
+		FB.getLoginStatus(function(response) {
+			if (response.status === 'connected') {
+				//TODO fazer o redirect
+			} else if (response.status === 'not_authorized') {
+				FB.login(SUPERTRUNFO.APPS.Facebook.login);
+			} else {
+				FB.login(SUPERTRUNFO.APPS.Facebook.login);
+			}
+			
+			FB.Canvas.setSize({width: 850});
+		});
 	}
-});
+};
+
+
+
